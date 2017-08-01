a472314161e9be908f6107441e0f7dfc593eb2a3
bauer-hillary-c <hbauer@ezcater.com>
2017-08-01 15:55:44 -0400

    adds API key

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyD9TNdVPFKQLxHodTgzNxuWLeArUPcK8RM';
+
 // Create a new component. This component should produce HTML
 const App = () => {
   return <div>Hi!</div>;
