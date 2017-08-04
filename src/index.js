b72312457019db700ef5fd963f78f0e64bb2d0b8
bauer-hillary-c <hbauer@ezcater.com>
2017-08-03 22:16:24 -0400

    switches app to a class component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
@@ -9,12 +9,14 @@ YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
 });
 
 // Create a new component. This component should produce HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
